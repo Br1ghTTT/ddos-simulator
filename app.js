fb1a968727e6c735ed4753592a8a394ac69dd6bc
Yura Horbachov <71140621+Br1ghTTT@users.noreply.github.com>
2021-01-25 13:28:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorController = require('./controller/error');
-const sequelize = require('./util/database');
+const errorController = require('./controllers/error');
+
+const sequelize = require('./util/database'); 
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 
-app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
 
 app.use(errorController.get404);
 
 sequelize.sync()
-.then(result => {
-    app.listen(8080);
-    console.log('Server is runnig at port: http://localhost:8080/');
-})
-.catch(err => {
-    console.log(err);
-})
-
+    .then(() => {
+        app.listen(8080);
+        console.log('Server is running on port http://localhost:8080/');
+    })
+    .catch(err =>{
+        console.log(err);
+    });
