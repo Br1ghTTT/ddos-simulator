8f047b525bbb7aa94ef547733abeef5cd120eb3c
Yura Horbachov <71140621+Br1ghTTT@users.noreply.github.com>
2021-01-26 14:49:41 +0200

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('ddos-simulator', 'root', '12345', {
+const sequelize = new Sequelize('ddos-simulator', 'root', '12345code22-105', {   // на місці ******* має бутии пароль до root в твому воркбенчі
     dialect: 'mysql',
-    host: 'localhost'
+    host: 'localhost',
+    pool: {
+        max: 100,
+        min:0,
+        idle: 200000,
+        acquire: 100*1000,
+    }
 });
 
 module.exports = sequelize;
