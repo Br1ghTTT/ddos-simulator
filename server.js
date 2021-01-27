58472388173fb27fe10487ec7e0cc90ac2fe26d4
Yura Horbachov <71140621+Br1ghTTT@users.noreply.github.com>
2021-01-27 22:04:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,252 +10,116 @@ app.use(bodyParser.urlencoded( {extended: false}) );
 
 const unirest = require('unirest');
 
-function sendRequest(i) {
+function sendRequest(){
+    for(let i = 0; i < 1000; i++){
+        setTimeout(async function() {
+            let req = unirest.post('http://localhost:8080/create-user')
+                .header('None', 'application/json')
+                .send({
+                    name: 'lala',
+                    description: 'ddos atack',
+                    date: d,
+                    latitude: 49.852,
+                    longitude: 23.94,
+                    date_deleted: d
+                })
+                .end(function () {
+                    console.log('Successfully!');
+                });
 
-    if( i > 0){
-        setTimeout(function() {
+            let req2 = unirest.post('http://localhost:8080/create-users')
+                .header('None', 'application/json')
+                .send({
+                    name: 'dala',
+                    description: 'ddos atack',
+                    date: d,
+                    latitude: 4932.833,
+                    longitude: 2334.998,
+                    date_deleted: d
+                })
+                .end(function () {
+                    console.log('Successfully!');
+                });
 
-        unirest.post('http://localhost:8080/create-user')
-            .header('None', 'application/json')
-            .send({
-            name: 'lala',
-            description: 'ddos atack',
-            date: d,
-            latitude: 49.85246326812933,
-            longitude: 23.998221548457174,
-            date_deleted: d
-            })
-            .end(function (response) {
-            console.log(response.body);
-            });
-            sendRequest(i+1)
-        }, 1); 
+            let req3 = unirest.post('http://localhost:8080/update-user')
+                .header('None', 'application/json')
+                .send({
+                    name: 'sala',
+                    description: 'ddos atack',
+                    date: d,
+                    latitude: 49.852,
+                    longitude: 23.994,
+                    date_deleted: d
+                })
+                .end(function () {
+                    console.log('Successfully!');
+                });
+
+            let req4 = unirest.post('http://localhost:8080/update-users')
+                .header('None', 'application/json')
+                .send({
+                    name: 'gala',
+                    description: 'ddos atack',
+                    date: d,
+                    latitude: 49.933,
+                    longitude: 24.974,
+                    date_deleted: d
+                })
+                .end(function () {
+                    console.log('Successfully!');
+                });
+
+            let req5 = unirest.post('http://localhost:8080/delete-user')
+                .header('None', 'application/json')
+                .send({
+                    name: 'hala',
+                    description: 'ddos atаck',
+                    date: d,
+                    latitude: 49.833,
+                    longitude: 23.964,
+                    date_deleted: d
+                })
+                .end(function () {
+                    console.log('Successfully!');
+                });
+
+            let req6 = unirest.post('http://localhost:8080/update-users')
+                .header('None', 'application/json')
+                .send({
+                    name: 'zala',
+                    description: 'ddos atack',
+                    date: d,
+                    latitude: 49.8523,
+                    longitude: 23.994,
+                    date_deleted: d
+                })
+                .end(function () {
+                    console.log('Successfully!');
+                });
+            
+            req++;
+            req2++;
+            req3++;
+            req4++;
+            req5++;
+            req6++;
+            return await req, req2, req3, req4, req5, req6;
+        }, 100); 
     }
 }
-    function sendRequest2(i) {
 
-        if( i > 0){
-            setTimeout(function() {
 
-            unirest.post('http://localhost:8080/create-users')
-            .header('None', 'application/json')
-            .send({
-            name: 'laфвфкорорla',
-            description: 'ddosвіфвфв atack',
-            date: d,
-            latitude: 4932.8524632681423423424232933,
-            longitude: 2334.998221548423423423457174,
-            date_deleted: d
-            })
-            .end(function (response) {
-            console.log(response.body);
-            });
-            sendRequest2(i+1)
-        }, 1); 
-    }
+async function asyncCall() {
+    await Promise.all([sendRequest()])
+        .then(() => {
+            console.log('Sent!');
+        })
+        .catch(err => {
+            console.log(err);
+        })
 }
-    function sendRequest3(i) {
 
-        if( i > 0){
-            setTimeout(function() {
-            unirest.post('http://localhost:8080/update-user')
-            .header('None', 'application/json')
-            .send({
-            name: 'lalвіаіаіаa',
-            description: 'ddos ваіаіаіаatack',
-            date: d,
-            latitude: 49.852463268123131231312933,
-            longitude: 23.998222131312313131548457174,
-            date_deleted: d
-            })
-            .end(function (response) {
-            console.log(response.body);
-            });
-            sendRequest3(i+1)
-        }, 1); 
-    }
-}
-    function sendRequest4(i) {
+asyncCall();
 
-        if( i > 0){
-            setTimeout(function() {
-            unirest.post('http://localhost:8080/update-users')
-            .header('None', 'application/json')
-            .send({
-            name: 'lalвіаваa',
-            description: 'ddos atackфвфв',
-            date: d,
-            latitude: 49.852463268432423412933,
-            longitude: 242343.998221548457174,
-            date_deleted: d
-            })
-            .end(function (response) {
-            console.log(response.body);
-            });
-            sendRequest4(i+1)
-        }, 1); 
-    }
-}
-    function sendRequest5(i) {
-
-        if( i > 0){
-            setTimeout(function() {
-
-            unirest.post('http://localhost:8080/delete-user')
-            .header('None', 'application/json')
-            .send({
-            name: 'lal242a',
-            description: 'ddos atаіack',
-            date: d,
-            latitude: 49.85246326234812933,
-            longitude: 23.9982211548457174,
-            date_deleted: d
-            })
-            .end(function (response) {
-            console.log(response.body);
-            });
-            sendRequest5(i+1)
-        }, 1); 
-    }
-}
-    function sendRequest6(i) {
-
-        if( i > 0){
-            setTimeout(function() {
-            unirest.post('http://localhost:8080/update-users')
-            .header('None', 'application/json')
-            .send({
-            name: 'lalу324авііa',
-            description: 'ddцуршіВos atack',
-            date: d,
-            latitude: 49.85246326323123142353563812933,
-            longitude: 23.99822265464572475421548457174,
-            date_deleted: d
-            })
-            .end(function (response) {
-            console.log(response.body);
-            });
-
-            sendRequest6(i+1)
-        }, 1); 
-    }
-}
-Promise.all([sendRequest(1), sendRequest2(1), sendRequest3(1), sendRequest4(1), sendRequest5(1), sendRequest6(1)])
-.then(() => {
-    console.log('Sent!');
-})
-.catch(err => {
-    console.log(err);
-})
-
-const server = app.listen(3000);
-server.setTimeout(5 * 1000000);
-console.log('Server is running at port: http://localhost:3000/');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* requestify.post('http://localhost:8080/create-users', {
-    name: 'lala',
-    description: 'ddos atack',
-    date: d,
-    latitude: 49.85246326812933,
-    longitude: 23.998221548457174,
-    date_deleted: d
-}).then(res => {
-    res.getBody();
-})
-.catch(err => {
-    console.log(err);
-}) */
-
-
-
-
-/* request.post(
-    "localhost:8080/create-user",
-    {
-        user:{
-            name: 'lala',
-            description: 'ddos atack',
-            date: d,
-            latitude: 49.85246326812933,
-            longitude: 23.998221548457174,
-            date_deleted: d
-        }
-    },
-    function (err, res, body){
-        console.log(body);
-    }
-); */
-
-/* var express = require('express');
-var querystring = require('querystring');
-var http = require('http');
-
-const d = new Date().getTime();
-
-var app = express();
-app.get('/', function (req, res) {
-  var data = querystring.stringify({
-    name: 'lala',
-    description: 'ddos atack',
-    date: d,
-    latitude: 49.85246326812933,
-    longitude: 23.998221548457174,
-    date_deleted: d
-  });
-
-  var options = {
-    host: 'http://localhost:8080/',
-    port: 8080,
-    path: '/create-users',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(data)
-    }
-  };
-
-  var httpreq = http.request(options, function (response) {
-    response.setEncoding('utf8');
-    response.on('data', function (chunk) {
-      console.log("body: " + chunk);
-    });
-    response.on('end', function() {
-      res.send('ok');
-    })
-  });
-  httpreq.write(data);
-  httpreq.end();
-});
 app.listen(3000);
-console.log('Server is running at port: http://localhost:3000/'); */
+console.log('Server is running at port: http://localhost:3000/');
